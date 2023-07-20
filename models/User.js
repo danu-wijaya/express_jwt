0b087bbd677b01a96dea9c9a209dd25ba7dc4532
kamandanu tresna wijaya <kamandanu.wijaya@gmail.com>
2023-07-20 10:50:48 +0700

    hash password

--- models/User.js
+++ models/User.js
@@ -1,27 +1,32 @@
-// Mengimpor modul mongoose untuk berinteraksi dengan MongoDB
-const mongoose = require("mongoose");
-
-// Mengimpor modul validator yang berisi fungsi isEmail untuk memvalidasi email
-const { isEmail } = require("validator");
+const mongoose = require('mongoose'); // Mengimpor modul mongoose untuk berinteraksi dengan MongoDB
+const { isEmail } = require('validator'); // Mengimpor fungsi isEmail dari modul validator untuk memvalidasi email
+const bcrypt = require('bcrypt'); // Mengimpor modul bcrypt untuk melakukan hashing password
 
 // Membuat skema (schema) untuk model User
 const userSchema = new mongoose.Schema({
   email: {
-    type: "string", // Tipe data untuk email adalah string
-    required: [true, "Please enter a valid email address"], // Email harus diisi dan harus merupakan alamat email yang valid
+    type: String, // Tipe data untuk email adalah string
+    required: [true, 'Please enter an email'], // Email harus diisi dan memiliki pesan error khusus jika tidak diisi
     unique: true, // Setiap email harus unik (tidak boleh ada duplikat)
     lowercase: true, // Mengubah email menjadi huruf kecil sebelum disimpan di database
-    validate: [isEmail, "Please enter a valid email address"], // Melakukan validasi dengan menggunakan fungsi isEmail dari validator
+    validate: [isEmail, 'Please enter a valid email'], // Melakukan validasi dengan menggunakan fungsi isEmail dari validator
   },
   password: {
-    type: "string", // Tipe data untuk password adalah string
-    required: [true, "Please enter a valid password"], // Password harus diisi
-    minlength: [6, "Minimum password length is 6 characters"], // Panjang password minimal harus 6 karakter
-  },
+    type: String, // Tipe data untuk password adalah string
+    required: [true, 'Please enter a password'], // Password harus diisi dan memiliki pesan error khusus jika tidak diisi
+    minlength: [6, 'Minimum password length is 6 characters'], // Panjang password minimal harus 6 karakter
+  }
+});
+
+// Fungsi middleware (pre hook) yang akan dijalankan sebelum dokumen disimpan ke database
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(); // Membuat "salt" untuk melakukan hashing password
+  this.password = await bcrypt.hash(this.password, salt); // Menghash password menggunakan salt yang telah dibuat
+  next(); // Melanjutkan proses penyimpanan dokumen ke database
 });
 
 // Membuat model User berdasarkan skema userSchema
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model('user', userSchema);
 
 // Ekspor model User agar bisa digunakan di berkas lain
 module.exports = User;
