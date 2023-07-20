38166104751a387fc016512806b310bde51b8e23
kamandanu tresna wijaya <kamandanu.wijaya@gmail.com>
2023-07-20 10:07:09 +0700

    mongoose validation

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,27 +1,53 @@
-const User = require("../models/User");
+const User = require("../models/User"); // Mengimpor model User dari berkas '../models/User'
 
+// Fungsi untuk menangani error
+const handleErrors = (err) => {
+  console.log(err.message, err.code); // Mencetak pesan error dan kode error ke konsol
+  let errors = { email: '', password: '' }; // Objek untuk menyimpan pesan error terkait email dan password
+
+  // Error duplikasi email
+  if (err.code === 11000) { // Jika kode error adalah 11000 (duplikasi)
+    errors.email = 'that email is already registered'; // Atur pesan error pada properti email
+    return errors; // Mengembalikan objek errors yang berisi pesan error
+  }
+
+  // Error validasi
+  if (err.message.includes('user validation failed')) { // Jika pesan error berisi "user validation failed"
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message; // Atur pesan error pada properti yang sesuai dengan field (path) yang salah
+    });
+  }
+
+  return errors; // Mengembalikan objek errors yang berisi pesan error
+};
+
+// Controller untuk menampilkan halaman signup (GET request)
 module.exports.signup_get = (req, res) => {
-  res.render("signup");
+  res.render('signup'); // Merender halaman "signup" untuk ditampilkan ke pengguna
 };
 
+// Controller untuk menampilkan halaman login (GET request)
 module.exports.login_get = (req, res) => {
-  res.render("login");
+  res.render('login'); // Merender halaman "login" untuk ditampilkan ke pengguna
 };
 
+// Controller untuk memproses data saat signup (POST request)
 module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body; // Mendapatkan data email dan password dari body request
 
   try {
-    const user = await User.create({ email, password });
-    res.status(201).json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({});
+    const user = await User.create({ email, password }); // Membuat user baru dengan menggunakan model User
+    res.status(201).json(user); // Mengirim respons berhasil dengan kode status 201 dan data user dalam format JSON
+  } catch (err) {
+    const errors = handleErrors(err); // Memanggil fungsi handleErrors untuk menangani error yang terjadi
+    res.status(400).json({ errors }); // Mengirim respons error dengan kode status 400 dan objek errors yang berisi pesan error
   }
 };
 
+// Controller untuk memproses data saat login (POST request)
 module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  res.json(req.body);
+  const { email, password } = req.body; // Mendapatkan data email dan password dari body request
+
+  console.log(email, password); // Mencetak data email dan password ke konsol
+  res.send('user login'); // Mengirim respons "user login"
 };
--- models/User.js
+++ models/User.js
@@ -1,20 +1,27 @@
+// Mengimpor modul mongoose untuk berinteraksi dengan MongoDB
 const mongoose = require("mongoose");
+
+// Mengimpor modul validator yang berisi fungsi isEmail untuk memvalidasi email
 const { isEmail } = require("validator");
 
+// Membuat skema (schema) untuk model User
 const userSchema = new mongoose.Schema({
   email: {
-    type: "string",
-    required: [true, "Please enter a valid email address"],
-    unique: true,
-    lowercase: true,
-    validate: [isEmail, "Please enter a valid email address"],
+    type: "string", // Tipe data untuk email adalah string
+    required: [true, "Please enter a valid email address"], // Email harus diisi dan harus merupakan alamat email yang valid
+    unique: true, // Setiap email harus unik (tidak boleh ada duplikat)
+    lowercase: true, // Mengubah email menjadi huruf kecil sebelum disimpan di database
+    validate: [isEmail, "Please enter a valid email address"], // Melakukan validasi dengan menggunakan fungsi isEmail dari validator
   },
   password: {
-    type: "string",
-    required: [true, "Please enter a valid password"],
-    minlength: [6, "Minimum password length is 6 characters"],
+    type: "string", // Tipe data untuk password adalah string
+    required: [true, "Please enter a valid password"], // Password harus diisi
+    minlength: [6, "Minimum password length is 6 characters"], // Panjang password minimal harus 6 karakter
   },
 });
 
+// Membuat model User berdasarkan skema userSchema
 const User = mongoose.model("user", userSchema);
+
+// Ekspor model User agar bisa digunakan di berkas lain
 module.exports = User;
